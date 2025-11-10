2a1ae2c45cf28eedb5959ab6e48a30dc5fed3518
jiangchou2366 <2580023589@qq.com>
2025-11-10 10:32:14 +0800

    11.10修改

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from app.routers import users
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(users.router, prefix="/api")
